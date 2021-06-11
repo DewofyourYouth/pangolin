c460723994f642bdd67dbe6a29150531803220de
Jacob Shore <jshore@datalign.net>
2021-06-11 13:10:43 +0300

    font change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Pangolin Software Solutions</h1>
+        <h1 className="title">Pangolin Software Solutions</h1>
       </header>
     </div>
   );
