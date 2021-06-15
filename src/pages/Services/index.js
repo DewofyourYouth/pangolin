526b06c46a0e07b9ec9ee9a107fde8d34bfae86d
Jacob Shore <jshore@datalign.net>
2021-06-15 14:28:38 +0300

    testing first

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -4,6 +4,11 @@ import "./Services.css";
 
 const services = [
   {
+    title: "Quality Testing & Assurance",
+    description:
+      "Eu labore amet magna officia tempor ullamco proident nisi consequat. Eiusmod ea qui ex adipisicing. Commodo ad irure nisi qui dolore sunt dolore dolor nulla. Laboris in dolore voluptate laboris occaecat ea amet non voluptate. Officia culpa sit eu ad veniam sit dolore qui eu aliquip veniam. Ullamco consectetur ex consequat adipisicing adipisicing magna. Aliqua proident aute deserunt ullamco tempor dolore.",
+  },
+  {
     title: "Develop A Web Solution",
     description:
       "Sunt nulla qui laboris et eu dolore. Eu consequat deserunt nulla labore eiusmod veniam Lorem. Officia cillum do sit eiusmod nostrud culpa fugiat dolor consectetur minim sint incididunt. Irure culpa minim sunt culpa quis ipsum reprehenderit consectetur cillum Lorem. Culpa laborum ea sint laboris deserunt fugiat ad irure culpa mollit velit magna commodo sit. Laborum irure pariatur labore ullamco enim culpa. \rIpsum eiusmod anim duis voluptate irure quis cupidatat cillum dolor est nulla velit aliquip do. Cillum incididunt deserunt laborum qui ut sint commodo. Magna ea excepteur laboris ex incididunt culpa incididunt amet sint occaecat amet id irure adipisicing. Laboris enim ex laboris commodo deserunt. Non non Lorem elit officia laborum aute enim et. Magna tempor cillum aliquip elit minim minim quis elit culpa magna enim consectetur.",
@@ -17,11 +22,6 @@ const services = [
     title: "Develop a Blockchain Solution",
     description:
       "Sint culpa nulla in ipsum. Aliqua et est et cupidatat consectetur minim culpa ullamco Lorem aliqua magna. Mollit aute quis sit deserunt amet aliquip sunt veniam. Irure cillum ad proident nisi proident commodo elit.",
-  },
-  {
-    title: "Quality Assurance & Testing",
-    description:
-      "Eu labore amet magna officia tempor ullamco proident nisi consequat. Eiusmod ea qui ex adipisicing. Commodo ad irure nisi qui dolore sunt dolore dolor nulla. Laboris in dolore voluptate laboris occaecat ea amet non voluptate. Officia culpa sit eu ad veniam sit dolore qui eu aliquip veniam. Ullamco consectetur ex consequat adipisicing adipisicing magna. Aliqua proident aute deserunt ullamco tempor dolore.",
   },
 ];
 
