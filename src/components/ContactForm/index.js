96bfcec48cfdc800691cc623f5f7c55d375f4694
Jacob Shore <jshore@datalign.net>
2021-06-17 13:04:13 +0300

    netlify form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,33 +1,44 @@
 import { Form, Button, Col } from "react-bootstrap";
 export function ContactForm() {
   return (
-    <Form variant="dark" className="text-white mb-5" netlify>
+    <Form
+      variant="dark"
+      className="text-white mb-5"
+      method="post"
+      data-netlify="true"
+    >
       <h2 className="title line-break-dark-bg">Contact Pangolin</h2>
-      <Form.Group controlId="email">
+      <Form.Group controlId="email" className="text-left mt-3">
         <Form.Label>Email address</Form.Label>
         <Form.Control
+          required
           type="email"
           name="email"
           placeholder="name@example.com"
         />
       </Form.Group>
-      <Form.Group>
+      <Form.Group className="text-left">
         <Form.Row>
           <Col>
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" name="firstName" placeholder="Joe" />
+            <Form.Label for="firstName">First Name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              name="firstName"
+              placeholder="Joe"
+            />
           </Col>
           <Col>
-            <Form.Label>Last Name</Form.Label>
+            <Form.Label for="lastName">Last Name</Form.Label>
             <Form.Control type="text" name="lastName" placeholder="Pangolin" />
           </Col>
         </Form.Row>
       </Form.Group>
-      <Form.Group controlId="Message">
+      <Form.Group controlId="Message" className="text-left">
         <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" name="message" rows={3} />
+        <Form.Control required as="textarea" name="message" rows={3} />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="outline-primary" type="submit">
         Submit
       </Button>
     </Form>
